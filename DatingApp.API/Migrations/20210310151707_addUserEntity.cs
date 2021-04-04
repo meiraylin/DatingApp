826ad18b7fe196af796a922736b59eea115cc74b
meiraylin <meiraylin@gmail.com>
2021-04-04 09:35:09 +0300

    End of section 3

--- DatingApp.API/Migrations/20210310151707_addUserEntity.cs
+++ DatingApp.API/Migrations/20210310151707_addUserEntity.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace DatingApp.API.Migrations
 {
-    public partial class User : Migration
+    public partial class addUserEntity : Migration
     {
-        public byte[] PasswordHash { get; internal set; }
-        public byte[] PasswordSalt { get; internal set; }
-
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
@@ -15,7 +13,9 @@ namespace DatingApp.API.Migrations
                 {
                     Id = table.Column<int>(nullable: false)
                         .Annotation("Sqlite:Autoincrement", true),
-                    name = table.Column<int>(nullable: false)
+                    Username = table.Column<string>(nullable: true),
+                    PasswordHash = table.Column<byte[]>(nullable: true),
+                    PasswordSalt = table.Column<byte[]>(nullable: true)
                 },
                 constraints: table =>
                 {
